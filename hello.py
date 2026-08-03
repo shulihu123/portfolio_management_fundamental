13c02a968f261f015360bae71c856be34641efb1
foxrabbit <foxrabbit@localhost>
2026-08-03 11:16:33 +0100

    Initial commit

--- hello.py
+++ hello.py
@@ -1,6 +1,42 @@
+
 import numpy as np
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-msg = "Roll a dice!"
-print(msg)
 
-print(np.random.randint(1,9))
+# msg = "Roll a dice!"
+# print(msg)
+# print(np.random.randint(1,9))
+
+# a = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [5,6,7]])
+# b = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+# c = np.array([[3,3,3], [9,9,9], [8,8,8]])
+# d = np.array([1,2,3,4,5,6,7,8,9])
+# e = np.arange(1,5)
+
+# print(e)
+
+# print(np.log2(e))
+
+
+x = np.gcd.reduce(([20, 8], [10, 6]), axis = 1)
+# array([10, 2])
+
+print(x)
+
+# def my_add(a, b):
+#     return a + b
+
+# for x in d:
+#     for y in d:
+#         if my_add(x, y) == 10:
+#             print(f"{x} + {y} = 10")
+
+# plt.show()
+# print(x)
+# print(y)
+
+
+
+
